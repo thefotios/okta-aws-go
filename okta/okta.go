abca9e47d66bd3cd61c7f75b94d5a343f427255c
Fotios Lindiakos <flindiakos@shutterstock.com>
2017-05-01 11:57:08 -0400

    Added the rest of the authn request params

--- okta/okta.go
+++ okta/okta.go
@@ -33,27 +33,51 @@ func New(hostname string, debug bool) *Okta {
 	return &o
 }
 
+// http://developer.okta.com/docs/api/resources/authn.html#authentication-transaction-modelhttp://developer.okta.com/docs/api/resources/authn.html#authentication-transaction-model
 type AuthenticationTransactionModel struct {
 	StateToken   string    `json:"stateToken"`
 	SessionToken string    `json:"sessionToken"`
 	ExpiresAt    time.Time `json:"expiresAt"`
-	Status       string    `json:"status"`
-	RelayState   string    `json:"relayState"`
-	FactorResult string    `json:"factorResult"`
+	// TODO: Status can be an Enum with custom marshalling: https://golang.org/pkg/encoding/json/#example__customMarshalJSON
+	Status       string `json:"status"`
+	RelayState   string `json:"relayState"`
+	FactorResult string `json:"factorResult"`
+}
+
+// http://developer.okta.com/docs/api/resources/authn.html#options-object
+type AuthnOptions struct {
+	MultiOptionalFactorEnroll bool `json:"multiOptionalFactorEnroll"`
+	WarnBeforePasswordExpired bool `json:"warnBeforePasswordExpired"`
+}
+
+// http://developer.okta.com/docs/api/resources/authn.html#context-object
+type AuthnContext struct {
+	DeviceToken string `json:"deviceToken"`
+}
+
+// http://developer.okta.com/docs/api/resources/authn.html#request-parameters
+type AuthnRequest struct {
+	Username   string       `json:"username"`
+	Password   string       `json:"password"`
+	RelayState string       `json:"relayState"`
+	Token      string       `json:"token"`
+	Options    AuthnOptions `json:"options"`
+	Context    AuthnContext `json:"context"`
 }
 
 // https://github.com/oktadeveloper/okta-aws-cli-assume-role/blob/master/src/main/java/com/okta/tools/awscli.java#L179
 func (okta Okta) PasswordLogin(username, password string) (*resty.Response, error) {
+	params := AuthnRequest{
+		Username: username,
+		Password: password,
+	}
 	resp, err := okta.client.R().
 		SetHeaders(map[string]string{
 			"Accept":        "application/json",
 			"Content-Type":  "application/json",
 			"Cache-Control": "no-cache",
 		}).
-		SetBody(map[string]string{
-			"username": username,
-			"password": password,
-		}).
+		SetBody(params).
 		SetResult(&AuthenticationTransactionModel{}).
 		Post("/authn")
 
